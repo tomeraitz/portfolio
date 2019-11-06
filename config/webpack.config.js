01858f07f07c5b84cfc6ea2ad2c509cddb9b639e
Tomer Raitz <tomeraitz1@gmail.com>
2019-11-06 23:06:09 +0200

    add compression

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -190,7 +190,7 @@ module.exports = function(webpackEnv) {
               // https://github.com/facebook/create-react-app/issues/2376
               // Pending further investigation:
               // https://github.com/mishoo/UglifyJS2/issues/2011
-              comparisons: false,
+              comparisons: true,
               // Disabled because of an issue with Terser breaking valid code:
               // https://github.com/facebook/create-react-app/issues/5250
               // Pending futher investigation:
