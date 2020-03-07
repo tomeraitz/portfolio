88eabad115f5f9e887a64eaef10c5806730557c0
Tomer Raitz <tomeraitz1@gmail.com>
2020-03-07 22:14:05 +0200

    added seo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,9 +14,9 @@ class Footer extends Component {
                                                     Linkedin, GitHub and StackOverFlow.
                     </div>
                     <div className="socialContiner">
-                        <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/tomer-raitz-13b64911b/" className="socailLinks Linkedin"><FaLinkedin /></a>
-                        <a rel="noopener noreferrer" target="_blank" href="https://github.com/tomeraitz" className="socailLinks"><FaGithub /></a>
-                        <a rel="noopener noreferrer" target="_blank" href="https://stackoverflow.com/users/10055871/tomer-raitz" className="socailLinks StackOverFlow"><FaStackOverflow /></a>
+                        <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/tomer-raitz-13b64911b/" aria-label="Linkedin" className="socailLinks Linkedin"><FaLinkedin /></a>
+                        <a rel="noopener noreferrer" target="_blank" href="https://github.com/tomeraitz" className="socailLinks" aria-label="GitHub"><FaGithub /></a>
+                        <a rel="noopener noreferrer" target="_blank" href="https://stackoverflow.com/users/10055871/tomer-raitz" className="socailLinks StackOverFlow" aria-label="StackOverFlow"><FaStackOverflow /></a>
                     </div>
                 </div>
         );
