d024fec5311fdc364427fc40e2f892e51a4c3a47
Tomer Raitz <tomeraitz1@gmail.com>
2020-12-27 18:15:08 +0200

    without pwa

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -49,7 +49,7 @@ export function register(config) {
         });
       } else {
         // Is not localhost. Just register service worker
-        registerValidSW(swUrl, config);
+        // registerValidSW(swUrl, config);
       }
     });
   }
